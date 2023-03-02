276502633f2b3fcd93a25bf33b8278f771c07357
kamranali <kamranalibusiness@gmail.com>
2023-03-02 16:53:48 -0700

    Update Setting feature

---
+++ src/test/java/com/helioteca/step_definitions/US10_StepDefs.java
@@ -0,0 +1,25 @@
+package com.helioteca.step_definitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US10_StepDefs {
+    @Given("user on the dashboard page")
+    public void userOnTheDashboardPage() {
+    }
+
+    @When("the user clicks the {string} module")
+    public void theUserClicksTheModule(String arg0) {
+    }
+
+    @And("user clicks Settings on the left bottom corner")
+    public void userClicksSettingsOnTheLeftBottomCorner() {
+    }
+
+    @Then("the user should be able to click any buttons")
+    public void theUserShouldBeAbleToClickAnyButtons() {
+    }
+
+}
