0ccca51783bd37677c2350ee26cf577a35d90ab8
Rauf <rauf.asgarov@gmail.com>
2023-03-07 14:29:53 -0500

    File Upload Test Completed

--- src/test/java/com/helioteca/runners/CukesRunner.java
+++ src/test/java/com/helioteca/runners/CukesRunner.java
@@ -13,7 +13,7 @@ import org.junit.runner.RunWith;
         },
         features = "src/test/resources/features",
         glue = "com/helioteca/step_definitions",
-        dryRun = true,
+        dryRun = false,
         tags = " @wip",
         publish = false //generating a report with public link
 )
--- src/test/java/com/helioteca/step_definitions/US07_StepDefs.java
+++ src/test/java/com/helioteca/step_definitions/US07_StepDefs.java
@@ -1,15 +1,21 @@
 package com.helioteca.step_definitions;
 
 import com.helioteca.pages.UploadFilePage_RA;
+import com.helioteca.utilities.BrowserUtils;
 import com.helioteca.utilities.Driver;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+import org.openqa.selenium.By;
 
-public class US07_StepDefs extends LoginStepDefs{
+import java.io.File;
+
+public class US07_StepDefs {
 
     UploadFilePage_RA uploadFilePageRa = new UploadFilePage_RA();
+
     @Given("user on the dashboard page")
     public void userOnTheDashboardPage() {
         Driver.getDriver().get("https://qa.helioteca.com/index.php/apps/files/?dir=/&fileid=2609");
@@ -17,19 +23,25 @@ public class US07_StepDefs extends LoginStepDefs{
 
     @When("the user clicks the {string} module")
     public void theUserClicksTheModule(String moduleName) {
-
+        uploadFilePageRa.navigateToModule(moduleName).click();
     }
 
 
     @When("the user clicks the add icon on the top")
     public void theUserClicksTheAddIconOnTheTop() {
+        uploadFilePageRa.uploadIconBtn.click();
+        BrowserUtils.sleep(2);
     }
+
 
     @And("users uploads file with the “upload file” option")
     public void usersUploadsFileWithTheUploadFileOption() {
+        String absolutePath = new File("src/test/resources/UploadFile_RA/UploadFile_RA.txt").getAbsolutePath();
+        uploadFilePageRa.uploadFileOpt.sendKeys(absolutePath);
     }
 
     @Then("verify the file is displayed on the page")
     public void verifyTheFileIsDisplayedOnThePage() {
+        Assert.assertTrue(uploadFilePageRa.uploadedFile.isDisplayed());
     }
 }
