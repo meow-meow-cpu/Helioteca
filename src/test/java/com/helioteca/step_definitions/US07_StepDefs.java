2e1a337d6d07c17ae350747585239ecb3eb05309
Rauf <rauf.asgarov@gmail.com>
2023-03-06 19:56:56 -0500

    Added Step Defs files and snippets

---
+++ src/test/java/com/helioteca/step_definitions/US07_StepDefs.java
@@ -0,0 +1,33 @@
+package com.helioteca.step_definitions;
+
+import com.helioteca.utilities.Driver;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class US07_StepDefs extends LoginStepDefs{
+
+    @Given("user on the dashboard page")
+    public void userOnTheDashboardPage() {
+        Driver.getDriver().get("https://qa.helioteca.com/index.php/apps/files/?dir=/&fileid=2609");
+    }
+
+    @When("the user clicks the {string} module")
+    public void theUserClicksTheModule(String arg0) {
+
+    }
+
+
+    @When("the user clicks the add icon on the top")
+    public void theUserClicksTheAddIconOnTheTop() {
+    }
+
+    @And("users uploads file with the “upload file” option")
+    public void usersUploadsFileWithTheUploadFileOption() {
+    }
+
+    @Then("verify the file is displayed on the page")
+    public void verifyTheFileIsDisplayedOnThePage() {
+    }
+}
