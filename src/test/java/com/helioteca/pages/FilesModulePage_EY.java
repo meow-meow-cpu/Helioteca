0b43c3a502e877d416c3b35d7a30c43108448fac
Emily_Yang <em.ek.yang@gmail.com>
2023-03-08 21:12:24 -0500

    US11 initial commit

---
+++ src/test/java/com/helioteca/pages/FilesModulePage_EY.java
@@ -0,0 +1,34 @@
+package com.helioteca.pages;
+
+import com.helioteca.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class FilesModulePage_EY {
+
+    public FilesModulePage_EY(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//ul[@id='appmenu']/li/a[@aria-label='Files']")
+    public WebElement files;
+
+    @FindBy(xpath = "//a[@data-action='menu']")
+    public WebElement action;
+
+    @FindBy(xpath = "//a[@data-action='Details']")
+    public WebElement details;
+
+    @FindBy(xpath = "//a[@id='commentsTabView']")
+    public WebElement comments;
+
+    @FindBy(xpath = "//div[@class='message']")
+    public WebElement commentsBox;
+
+    @FindBy(xpath = "//input[@class='confirm']")
+    public WebElement send;
+
+    @FindBy(xpath = "//div[@id='commentsTabView']/ul/li[@class='comment']")
+    public WebElement commentPosted;
+}
---
+++ src/test/java/com/helioteca/step_definitions/US11StepDefs_EY.java
@@ -0,0 +1,47 @@
+package com.helioteca.step_definitions;
+
+import com.helioteca.pages.FilesModulePage_EY;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+public class US11StepDefs_EY {
+
+    FilesModulePage_EY filesModulePage_ey = new FilesModulePage_EY();
+    @Then("the user should be on the dashboard")
+    public void the_user_should_be_on_the_dashboard() {
+    }
+
+    @When("the user clicks on the Files module")
+    public void the_user_clicks_on_the_files_module() {
+        filesModulePage_ey.files.click();
+    }
+
+    @Then("the user clicks on action icon from any file on the page")
+    public void the_user_clicks_on_action_icon_from_any_file_on_the_page() {
+        filesModulePage_ey.action.click();
+    }
+
+    @Then("the user chooses the Details option")
+    public void the_user_chooses_the_details_option() {
+        filesModulePage_ey.details.click();
+    }
+
+    @Then("the user writes a comment {string} inside the input box")
+    public void the_user_writes_a_comment_inside_the_input_box(String comment) {
+        filesModulePage_ey.comments.click();
+        filesModulePage_ey.commentsBox.sendKeys(comment);
+    }
+
+    @Then("the user clicks the submit button to post it")
+    public void the_user_clicks_the_submit_button_to_post_it() {
+        filesModulePage_ey.send.click();
+    }
+
+    @Then("verify the comment {string} is displayed in the comment section")
+    public void verify_the_comment_is_displayed_in_the_comment_section(String comment) {
+        Assert.assertTrue(filesModulePage_ey.commentPosted.isDisplayed());
+    }
+
+}
